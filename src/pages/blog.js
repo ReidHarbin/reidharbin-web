35f38027a926c205d47f6f59f0d660f0c9138fe7
ReidHarbin <harbinreidt@gmail.com>
2023-08-22 22:18:01 -0400

    Added under construction display.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHelmetSafety } from "@fortawesome/free-solid-svg-icons";
 
 const Blog = () => {
     return (
-        <h1>
-            Blog
-        </h1>
+        <div className="flex items-center justify-center h-screen bg-slate-900 text-white">
+            <div className="text-center">
+                <div className="md:text-8xl">
+                    <FontAwesomeIcon icon={faHelmetSafety} />
+                </div>
+                <p>
+                    This site is under construction
+                </p>
+            </div>
+        </div>
+
     )
 }
 
