33912d0aaa84b4cbe38240d6b2e2fd4168235c1d
ReidHarbin <harbinreidt@gmail.com>
2023-08-23 00:16:19 -0400

    Increased security.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,34 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faAt, faR, faE, faI, faD, faH, faA,faB, faN, faT, faG, faM, faL, faC, faO, faCircle } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
     return (
         <div className="bg-slate-900 grow text-white flex items-center justify-center">
-            <div className="flex flex-col place-content-center">
-                <div className="md:text-8xl text-center">
-                    harbinreidt@gmail.com
+            <div className="flex flex-col place-content-center text-lg md:text-7xl">
+                <div className="text-center font-bold">
+                    <FontAwesomeIcon className="pl-1" icon={faH}/>
+                    <FontAwesomeIcon className="pl-1" icon={faA}/>
+                    <FontAwesomeIcon className="pl-1" icon={faR}/>
+                    <FontAwesomeIcon className="pl-1" icon={faB}/>
+                    <FontAwesomeIcon className="pl-1" icon={faI}/>
+                    <FontAwesomeIcon className="pl-1" icon={faN}/>
+                    <FontAwesomeIcon className="pl-1" icon={faR}/>
+                    <FontAwesomeIcon className="pl-1" icon={faE}/>
+                    <FontAwesomeIcon className="pl-1" icon={faI}/>
+                    <FontAwesomeIcon className="pl-1" icon={faD}/>
+                    <FontAwesomeIcon className="pl-1" icon={faT}/>
+                    <FontAwesomeIcon className="pl-1"icon={faAt}/>
+                    <FontAwesomeIcon className="pl-1" icon={faG}/>
+                    <FontAwesomeIcon className="pl-1" icon={faM}/>
+                    <FontAwesomeIcon className="pl-1" icon={faA}/>
+                    <FontAwesomeIcon className="pl-1" icon={faI}/>
+                    <FontAwesomeIcon className="pl-1" icon={faL}/>
+                    <FontAwesomeIcon className="pl-1 text-sm" icon={faCircle}/>
+                    <FontAwesomeIcon className="pl-1" icon={faC}/>
+                    <FontAwesomeIcon className="pl-1" icon={faO}/>
+                    <FontAwesomeIcon className="pl-1" icon={faM}/>
                 </div>
                 <div className="flex md:text-8xl pt-4 justify-center">
                     <div className="mr-2">
