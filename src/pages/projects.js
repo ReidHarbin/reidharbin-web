92fe61e4de538b9eb1d0950f0252fc2a3fc8a1ef
ReidHarbin <harbinreidt@gmail.com>
2023-08-29 05:32:45 -0400

    Updated styling.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,37 +9,46 @@ import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const project = (title, githubLink, websiteLink, backendTools, frontendTools, photo, description) => {
     return (
-        <div className="text-white w-fit">
-            <div className="flex items-center justify-center rounded-lg overflow-hidden mb-4 h-1/2">
-                <img  className="w-full" src={photo} alt="thumbnail"></img>
-            </div>
-            <div className=" bg-gray-700 rounded-xl p-2 text-md h-fit">
-                <h1 className="text-3xl">{title}</h1>
-                    <p className="pb-2">
-                        Backend: {backendTools}
-                    </p>
-                    {(frontendTools !== null) ?
-                        <p1 className="pb-2">
-                            Frontend: {frontendTools}
-                        </p1> :
-                        null    
-                    }
-                    <div className="text-3xl border-b-2 py-2">
-                        <a className="hover:text-green-500 pr-2" href={githubLink} target="_blank" rel="noopener noreferrer">
-                            <FontAwesomeIcon icon={faGithub} />
-                        </a>
-                        {(websiteLink !== null ) ? 
-                            <a className="hover:text-slate-900" href={websiteLink} target="_blank" rel="noopener noreferrer">
-                                <FontAwesomeIcon icon={faUpRightFromSquare}/>
-                            </a> :
-                            null
+        <div className="grow text-white w-fit m-4 rounded-xl">
+            <div className="shadow-lg shadow-gray-900 rounded-xl">
+                <div className="flex items-center justify-center rounded-t-lg overflow-hidden h-1/3">
+                    <img  className="w-full" src={photo} alt="thumbnail"></img>
+                </div>
+                <div className=" bg-gray-700 rounded-b-xl p-2">
+                    <div className="flex text-2xl md:text-3xl mb-2">
+                        <h1 >{title}</h1>
+                        <div className="">
+                            <a className="hover:text-green-500 mx-2" href={githubLink} target="_blank" rel="noopener noreferrer">
+                                <FontAwesomeIcon icon={faGithub} />
+                            </a>
+                            {(websiteLink) ? 
+                                <a className="hover:text-gray-500" href={websiteLink} target="_blank" rel="noopener noreferrer">
+                                    <FontAwesomeIcon icon={faUpRightFromSquare}/>
+                                </a> :
+                                null
+                            }
+                        </div>
+                    </div>
+                    <div className="text-sm">
+                        {(backendTools) ?
+                            <p className="">
+                                Backend: {backendTools}
+                            </p> :
+                            null    
+                        }
+                        {(frontendTools) ?
+                            <p1 className="">
+                                Frontend: {frontendTools}
+                            </p1> :
+                            null    
                         }
                     </div>
-                    <div>
-                        <p className="pt-2">
-                            {description}
-                        </p>
-                    </div>
+                        
+                    <p className="grow text-sm md:text-md border-t-2 pt-2 mt-2">
+                        {description}
+                    </p>
+                        
+                </div>
             </div>
         </div>
     )
@@ -49,15 +58,18 @@ const project = (title, githubLink, websiteLink, backendTools, frontendTools, ph
 
 const Projects = () => {
     return (
-        <div className="bg-slate-900">
-            <div className="grow grid grid-cols-1 md:grid-cols-3 gap-4 mx-4">
+        <div className="bg-gray-500">
+            <div className="grow flex flex-col col-auto grid-cols-1 md:grid md:grid-cols-3 gap-4">
                 {project("TouchBase", 
                     "https://github.com/ReidHarbin/TouchBase", 
                     "http://touchbase.s3-website-us-west-2.amazonaws.com/",
                     "Java | AWS APIGateway | AWS Lambda | AWS DynamoDB | AWS CloudFormation",
                     "Javascript | React | Tailwindcss | AWS S3",
                     TouchBasePhoto,
-                    "This will be the description")}
+                    "TouchBase is a robust REST API crafted to power a family event management application. "  +
+                    "Through a well-considered integration of AWS services - API Gateway, Lambda, and DynamoDB " + 
+                    "- I orchestrated a streamlined data flow from client to storage. Throughout its development, " +
+                    "I adhered to essential OOP principles and leveraged design patterns to ensure a scalable and maintainable codebase.")}
                     
                 {project("Kindle Publishing Service", 
                     "https://github.com/BloomTechBackend/bd-kindle-publishing-service-ReidHarbin", 
@@ -77,7 +89,7 @@ const Projects = () => {
                 {project("reidharbin.com", 
                     "https://github.com/ReidHarbin/reidharbin-web", 
                     null,
-                    "Java | AWS APIGateway | AWS Lambda | AWS DynamoDB | AWS CloudFormation",
+                    null,
                     "Javascript | React | Tailwindcss | AWS S3 | AWS Route53 | AWS CloudFront",
                     MusicServicePhoto,
                     "this will be the description")}
