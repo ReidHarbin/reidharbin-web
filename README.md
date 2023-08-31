62e3662cdec743522c17b93966ae6d881ad59216
ReidHarbin <harbinreidt@gmail.com>
2023-08-30 20:12:52 -0400

    Updated README.

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# Getting Started with Create React App
+---
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
+---
 # Reid Harbin
 
 This is the code for my personal website.
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TouchBasePhoto from "./images/touchbase-stock-photo.jpg";
+import TouchBasePhoto from "./images/touchbase-stock-blue.jpg";
 import KindleServicePhoto from "./images/kindle-service.png";
 import MusicServicePhoto from "./images/amazon-music.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,13 +9,13 @@ import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const project = (title, githubLink, websiteLink, backendTools, frontendTools, photo, description) => {
     return (
-        <div className="grow text-white m-4 rounded-xl">
+        <div className="grow text-white mt-2 mb-8 mx-2 rounded-xl">
             <div className="shadow-lg shadow-gray-900 rounded-xl">
-                <div className="flex items-center justify-center rounded-t-lg overflow-hidden h-1/3">
-                    <img  className="w-full" src={photo} alt="thumbnail"></img>
+                <div className="flex justify-center rounded-t-lg overflow-hidden max-h-56 w-full">
+                    <img  className="" src={photo} alt="thumbnail"></img>
                 </div>
                 <div className=" bg-gray-600 rounded-b-xl p-2">
-                    <div className="flex text-xl md:text-2xl mb-2">
+                    <div className="flex text-2xl md:text-2xl mb-2">
                         <h1 >{title}</h1>
                         <div className="">
                             <a className="hover:text-green-500 mx-2" href={githubLink} target="_blank" rel="noopener noreferrer">
@@ -30,7 +30,7 @@ const project = (title, githubLink, websiteLink, backendTools, frontendTools, ph
                         </div>
                     </div>
                     <div className="font-Archivo">
-                        <div>
+                        <div className="font-ArchivoBlack text-md">
                             {(backendTools) ?
                                 <h2 className="mb-2">
                                     Backend: {backendTools}
@@ -44,8 +44,7 @@ const project = (title, githubLink, websiteLink, backendTools, frontendTools, ph
                                 null    
                             }
                         </div>
-                            
-                        <p className="grow text-sm md:text-lg border-t-2 pt-2 mt-2">
+                        <p className="grow text-lg border-t-2 pt-2 mt-2">
                             {description}
                         </p>
                     </div>
@@ -59,10 +58,10 @@ const project = (title, githubLink, websiteLink, backendTools, frontendTools, ph
 
 const Projects = () => {
     return (
-        <div className="bg-gray-700">
+        <div className="bg-gray-700 grow">
             <div className="grow">
                 <div className="md:flex md:flex-grid md:grid-col-3">
-                    <div className="md:w-1/3">
+                    <div className="mx-2 md:w-1/3">
                         {project("TouchBase", 
                             "https://github.com/ReidHarbin/TouchBase", 
                             "http://touchbase.s3-website-us-west-2.amazonaws.com/",
@@ -80,9 +79,9 @@ const Projects = () => {
                             null,
                             "Javascript | React | Tailwindcss | AWS S3 | AWS Route53 | AWS CloudFront",
                             MusicServicePhoto,
-                            "this will be the description")}
+                            "This is a website that I made from scratch to showcase my work as a Backend Developer")}
                     </div>
-                    <div className="md:w-1/3">
+                    <div className="mx-2 md:w-1/3">
                         {project("Kindle Publishing Service", 
                             "https://github.com/BloomTechBackend/bd-kindle-publishing-service-ReidHarbin", 
                             null,
@@ -92,12 +91,11 @@ const Projects = () => {
                             "This project aimed to simulate the creation of a new Amazon Kindle Publishing Service. " +
                             "I deciphered complex documentation to create UML Class Diagrams to help understand existing code and ensure the service meets the proper design. " +
                             "I used soft deletion logic on the CataglogItemVersion DynamoDB Table to ensure data preservation while maintaining integrity of the service's operations. " +
-                            "Signigicantly enhanced the services effiency by implementing threads resulting in faster response times for the client. " +
-                            "Showcased my ability to implement threads which increased the efficiency of the service. " +
-                            "I adeptly formulated DynamoDB queries to enhance essential interactions with DynamoDB. "
+                            "I signigicantly enhanced the services effiency by implementing threads resulting in faster response times for the client. " +
+                            "I adeptly formulated DynamoDB queries to enhance essential interactions with client requests and data. "
                             )}
                     </div>
-                    <div className="md:w-1/3">
+                    <div className="mx-2 md:w-1/3">
                         {project("Music Playlist Service", 
                             "https://github.com/BloomTechBackend/bd-working-with-databases-music-playlist-service-ReidHarbin", 
                             null,
